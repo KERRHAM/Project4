8766db5de2c9f1c72a434170067343dc895b03c1
Kerr Hamilton <kerr9928@gmail.com>
2025-01-31 21:32:18 +0000

    Create app and test on server

--- ice_bath/asgi.py
+++ ice_bath/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Ice_bath project.
+ASGI config for ice_bath project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ice_bath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ice_bath.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ice_bath.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ice_bath.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
