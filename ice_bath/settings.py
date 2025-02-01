aa33653bc4b86eb16b8829b22c653c8e80be6dfb
Kerr Hamilton <kerr9928@gmail.com>
2025-02-01 14:53:20 +0000

    Create post model

--- ice_bath/settings.py
+++ ice_bath/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u6j%v*!#06@y_j4fcl()dg(=%gaz2-bqug_%@5bkr-3&(2e(h0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kerrham-project4-d69dw4cn6up.ws-eu117.gitpod.io', 
                  '.herokuapp.com']
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'ice_bath.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 
